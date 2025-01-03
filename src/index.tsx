8db0c0e72b71876a4f5b7410ddb7092db4fc0b50
TaniaNovikova <tania.novikov@web.de>
2025-01-03 04:25:20 +0100

    add font Lato

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "@fontsource/lato"; // Подключает все веса по умолчанию
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
